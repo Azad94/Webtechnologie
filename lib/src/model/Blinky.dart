d0d4dd52e92c71d1d2a3fb231d76b0d4779b35e0
july <juliane.steglich@web.de>
2016-05-11 16:57:39 +0200

    rekursives Pathfinding, fehlendes Testen, da ich die Schnittstellen nicht kenne

--- lib/src/model/Blinky.dart
+++ lib/src/model/Blinky.dart
@@ -1,67 +1,149 @@
 part of pacmanLib;
 
+//shadow
 class Blinky extends Ghost {
+  Level _level;
+  bool eatable = false;
+
+  int x1 = 5;
+  int y1 = 5;
+  int x2 = 1;
+  int y2 = 4;
+
   Blinky(int x, int y, bool collPlayer, bool collGhost, Level l)
       : super(x, y, collPlayer, collGhost, l);
 
-  void move() {}
-  void eatableMode() {}
-
-  int getXPacMan = 3;
-  int getYPacMan = 3;
-
-  int start = 0;
-
-  int x = 4;
-  int y = 4;
-
-  // weiterer Test
-
-/*solange ShadowXY != PacXY ist, sollen die for-Schleifen die Werte hochzählen
-  Muss nach jedem Durchlauf aktuelle Pac Postion abfragen
-  !!!Problem, man muss evlt. runterzählen, also variabel mal rauf mal runter, wie unterscheiden????
-  bricht ab bei PowerPill, dann soll shadow langsamer werden
-  wenn gefressen, dann ins Haus
-  ScatterMode und InvsibleMode verschiedene Strategien unterscheiden
-
-*/
-  void PacManRun(int getXPacMan, int getYPacMan) {
-    for (int x = 0; x < 100; x++) {
-      getXPacMan = x;
-      for (int y = 0; y < 100; y++) {
-        getYPacMan = y;
-      }
-    }
-
-    while (getYPacMan != x) {
-      if (getXPacMan < x) {
-        for (int i = 0; i < getXPacMan; i++) {
-          i = x;
-          if (getYPacMan < y) {
-            for (int j = 0; j < getYPacMan; j++) {
-              j = y;
-            }
-          } else {
-            for (int k = 0; k < getYPacMan; k--) {
-              k = y;
-            }
-            if (getXPacMan < x) {
-              for (int l = 0; l < getXPacMan; l++) {
-                l = x;
-                if (getYPacMan < y) {
-                  for (int m = 0; m < getYPacMan; m++) {
-                    m = y;
-                  }
-                } else {
-                  for (int n = 0; n < getYPacMan; n--) {
-                    n = y;
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+//gibt die bisher beste Liste zurück
+  List<Direction> getRoute() {
+    List<Direction> route = new List();
+    return getRouteRecursive(route, _x, _y);
   }
+
+  List<Direction> getRouteRecursive(
+      List<Direction> prev, int routeX, int routeY) {
+    if (prev.length > 10) {
+      return prev;
+    }
+
+    List<Direction> left;
+    List<Direction> up;
+    List<Direction> right;
+    List<Direction> down;
+    List<Direction> best = new List();
+
+    // requires better interface to differentiate collisions from finding pacman
+    if ((prev.length == 0 || prev.last != Direction.RIGHT) &&
+        !_level.checkCollision(routeX + 1, routeY, this)) {
+      if (routeX + 1 == x1 && routeY == y1) {
+        prev.add(Direction.RIGHT);
+        right = prev;
+      } else {
+        prev.add(Direction.RIGHT);
+        right = getRouteRecursive(prev, routeX + 1, routeY);
+      }
+    }
+    if ((prev.length == 0 || prev.last != Direction.UP) &&
+        !_level.checkCollision(routeX, routeY + 1, this)) {
+      if (routeX == x1 && routeY + 1 == y1) {
+        prev.add(Direction.UP);
+        up = prev;
+      } else {
+        prev.add(Direction.UP);
+        up = getRouteRecursive(prev, routeX, routeY + 1);
+      }
+    }
+    if ((prev.length == 0 || prev.last != Direction.LEFT) &&
+        !_level.checkCollision(routeX - 1, routeY, this)) {
+      if (routeX - 1 == x1 && routeY == y1) {
+        prev.add(Direction.LEFT);
+        left = prev;
+      } else {
+        prev.add(Direction.LEFT);
+        left = getRouteRecursive(prev, routeX - 1, routeY);
+      }
+    }
+    if ((prev.length == 0 || prev.last != Direction.DOWN) &&
+        !_level.checkCollision(routeX, routeY - 1, this)) {
+      if (routeX + 1 == x1 && routeY == y1) {
+        prev.add(Direction.DOWN);
+        down = prev;
+      } else {
+        prev.add(Direction.DOWN);
+        down = getRouteRecursive(prev, routeX, routeY - 1);
+      }
+    }
+//Vergleich der Listen um den kürzesten Weg zu finden
+    if (left != null) {
+      best = left;
+    }
+    if (up != null && (best.length == 0 || up.length < best.length)) {
+      best = up;
+    }
+    if (right != null && (best.length == 0 || right.length < best.length)) {
+      best = rigth;
+    }
+    if (down != null && (best.length == 0 || down.length() < best.length)) {
+      best = down;
+    }
+    return best;
+  }
+
+  // @override
+  // bool getNextMove(currentX, currentY, targetX, targetY) {
+// getRoute().first;
+}
+
+void eatableMode() {}
+
+void calcRoute() {}
+
+void move() {
+  dir = getRoute().first;
+  if (dir == Directions.UP) {
+    if (!_level.checkCollision(_x, _y - 1, this))
+      _level.registerElement(_x, _y, _x, --_y, this);
+  }
+  if (dir == Directions.DOWN) {
+    if (!_level.checkCollision(_x, _y + 1, this))
+      _level.registerElement(_x, _y, _x, ++_y, this);
+  }
+  if (dir == Directions.LEFT) {
+    if (!_level.checkCollision(_x - 1, _y, this))
+      _level.registerElement(_x, _y, --_x, _y, this);
+  }
+  if (dir == Directions.RIGHT) {
+    if (!_level.checkCollision(_x + 1, _y, this))
+      _level.registerElement(_x, _y, ++_x, _y, this);
+  }
+
+  /*switch(d)
+  {
+    case Direction.UP:
+
+        if((checkCollision(bx,by+1))==false){
+        by++;
+        }
+        break;
+
+    case Direction.DOWN:
+
+      if((checkCollision(bx,by-1))==false){
+        by--;
+        }
+         break;
+
+    case Direction.LEFT:r
+
+         if((checkCollision(bx-1,by))==false){
+          bx--;
+         }
+        break;
+
+    case Direction.RIGHT:
+
+      if((checkCollision(bx+1,by))==false){
+          bx++;
+         }
+         break;
+  }*/
 }
--- lib/src/model/Ghost.dart
+++ lib/src/model/Ghost.dart
@@ -1,10 +1,330 @@
 part of pacmanLib;
 
-abstract class Ghost extends GameElement{
+abstract class Ghost extends GameElement {
+
+  double speed = 0.0;
+  bool eatable = false;
+  bool invisible = true;
+  int start = 0;
+  int frightenedModeTimer = 0;
+  bool scatter;
+  final int chaseMode = 200;
+  final int scatterMode = 50;
+  int countDownTimer;
+  Directions nextDirection;
+  Directions _previousDirection = Directions.LEFT;
+  Directions _savePreviousDirection;
+  int _ghostsEaten = 0;
   Level _level;
 
   Ghost(int x, int y, bool collPlayer, bool collGhost, Level l) : super(x, y, collPlayer, collGhost), this._level = l;
 
   void move();
   void eatableMode();
+
+  //8 X 7 DUMMY MAP
+  List<List> dummyMap =[
+    [00,10,20,30,40,50,60],//00
+    [01,11,21,31,41,51,61],
+    [02,12,22,32,42,52,62],
+    [03,13,23,33,43,53,63],
+    [04,14,24,34,44,54,64],//24
+    [05,15,25,35,45,55,65],
+    [06,16,26,36,46,56,66],
+    [07,17,27,37,74,57,67],
+  ];
+
+  //intializes the default START and SCATTER POSITIONS
+  void initDefaultPosition(_positions, _dummyMap)//
+  {
+    //erst Spalte dann Zeile, das sind auf einem 8x6 Feld die passenden Positionen
+
+    //BLINKY Start Position
+    _dummyMap[4][4] = Dynamics.BLINKY;
+    //PINKY Start Position
+    _dummyMap[4][5] = Dynamics.PINKY;
+    //INKY Start Position
+    _dummyMap[3][5] = Dynamics.INKY;
+    //CLYDE Start Position
+    _dummyMap[5][5] = Dynamics.CLYDE;
+  }
+  /**
+      void initScatterPosition(_dummyMap)
+      {
+      //BLINKY SCATTER POSITION
+      _dummyMap[6][0] = Statics.SBLINKY;
+      //PINKY SCATTER POSITION
+      _dummyMap[0][0] = Statics.SPINKY;
+      //INKY SCATTER POSITION
+      _dummyMap[6][7] = Statics.SINKY;
+      //CLYDE SCATTER POSITION
+      _dummyMap[0][7] = Statics.SCLYDE;
+      }
+   **/
+  List<List> findScatterPosition(_dummyMap)
+  {
+    List<List> _scatterPosition = [6][7];
+    return _scatterPosition;
+  }
+
+  List<List> getCurrentPosition()
+  {
+    List<List> _currentPosition = [3][5];
+    return _currentPosition;
+  }
+
+  void setWalls(_dummyMap)
+  {
+    _dummyMap[4][5] = Statics.WALL;
+    _dummyMap[5][5] = Statics.WALL;
+    _dummyMap[4][6] = Statics.WALL;
+    _dummyMap[5][6] = Statics.WALL;
+  }
+
+  /**
+   * if the ghosts are eatable, return value is set to false
+   * if the ghosts are not eatable, return value is set to true
+   **/
+  void setEatable()
+  {
+    eatable ? eatable = true : eatable = false;
+  }
+
+  //return's true if the ghosts are eatable else false
+  bool isEatable()
+  {
+    return eatable;
+  }
+
+  /**
+      void setDirection(Directions dir)
+      {
+      _previousDirection = nextDirection;
+      nextDirection = dir;
+      }
+   **/
+
+  //returns true if the ghosts are in Scatter Mode
+  bool isScatterModeOn()
+  {
+    return scatter;
+  }
+
+  void startScatterMode()
+  {
+    //TODO muss noch implementiert werden, Logik fehlt noch, muss ich mir noch überlegen
+    scatter = true;
+    int _scatterTimer = 7;
+
+    while (_scatterTimer > 0)
+    {
+      _scatterTimer--;
+    }
+  }
+
+  void stopScatter()
+  {
+    //TODO muss noch implementiert werden, Logik fehlt noch, muss ich mir noch überlegen
+    scatter = false;
+
+  }
+
+  //as param give the ghost to be respawned
+  void respawn(Dynamics ghost){
+    //TODO muss noch geklärt werden auf wessen seite das passiert auf meiner oder der von niklas
+  }
+
+  //abhängig davon wieviele Ghosts schon gefressen wurden
+  int getScoreValue()
+  {
+    _ghostsEaten++;
+    return _ghostsEaten;
+  }
+
+  //sets the score of the ghosts to zero
+  void setGhostScoreToZero()
+  {
+    _ghostsEaten = 0;
+  }
+
+  int getDoorX()
+  {
+    int x = 3;
+    return x;
+  }
+
+  int getDoorY()
+  {
+    int y = 4;
+    return y;
+  }
+
+  bool getNextMove(currentX, currentY, targetX, targetY)
+  {
+    //horizontal difference from currentPosition to targetPosition
+    int horDifference = currentX-targetX;
+    int verDifference = currentY-targetY;
+
+    //preferred direction considering the difference     UP-> LEFT-> DOWN
+    Directions preferredHorDirection = horDifference > 0 ? Directions.LEFT : Directions.RIGHT;
+    Directions preferredVerDirection = verDifference > 0 ? Directions.UP : Directions.DOWN;
+
+    //checks if the vertical difference is greater than the horizontal
+    bool verticalMoreImportant = verDifference.abs() > horDifference.abs();
+
+    //sets next preferred direction
+    verticalMoreImportant ? nextDirection = preferredVerDirection : nextDirection = preferredHorDirection;
+
+
+    if(verticalMoreImportant)
+    {
+      if(_previousDirection == Directions.UP || _previousDirection == Directions.DOWN)
+      {
+        _savePreviousDirection = _previousDirection;
+
+        nextDirection == _previousDirection ? nextDirection : nextDirection = Directions.LEFT;
+        if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+        switch(nextDirection)
+        {
+          case Directions.UP :
+            nextDirection = Directions.LEFT;
+            if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+            nextDirection = Directions.RIGHT;
+            if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+            break;
+
+          case Directions.DOWN :
+            nextDirection = Directions.LEFT;
+            if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+            nextDirection = Directions.RIGHT;
+            if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+            break;
+
+          case Directions.LEFT :
+            if(_previousDirection == Directions.UP)
+            {
+              nextDirection = Directions.RIGHT;
+              if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+              nextDirection = Directions.DOWN;
+              if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+            }
+            else
+            {
+              nextDirection = Directions.RIGHT;
+              if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+              nextDirection = Directions.UP;
+              if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+            }
+            break;
+
+          default:
+            Directions.RIGHT;
+            break;
+        }
+      }
+    }
+    else
+    { //if the next and previous Direction are same nothing changes
+      if(_previousDirection == Directions.LEFT || _previousDirection == Directions.RIGHT) {
+        _savePreviousDirection = _previousDirection;
+
+        //if they are different, try going UP first
+        nextDirection == _previousDirection ? nextDirection : nextDirection = Directions.UP;
+        if (!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+        if (nextDirection == Directions.UP)
+        {
+          if(_savePreviousDirection == Directions.LEFT)
+          {
+            nextDirection = Directions.DOWN;
+            if (!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+            if (nextDirection == Directions.DOWN && preferredHorDirection == Directions.RIGHT)
+            {
+              nextDirection = Directions.RIGHT;
+              if (!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+            }
+          }
+          else
+          {
+            if(_savePreviousDirection == Directions.RIGHT && preferredHorDirection == Directions.LEFT)
+            {
+              nextDirection = Directions.DOWN;
+              if (!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+              nextDirection = Directions.LEFT;
+              if (!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+              return false;
+            }
+            //TODO is this return still needed, i mean there is no possibility where you can get stucked
+            return false;
+          }
+        }
+
+        //if RIGHT is not allowed try going UP
+        if(nextDirection == Directions.RIGHT && _savePreviousDirection == Directions.RIGHT && preferredHorDirection == Directions.RIGHT)
+        {
+          nextDirection = Directions.UP;
+          if(!isMoveAllowed(nextDirection, currentX,currentY)) return true;
+
+          //if UP not allowed try going DOWN
+          nextDirection = Directions.DOWN;
+          if(!isMoveAllowed(nextDirection, currentX, currentY)) return true;
+
+          //TODO is this return still needed, i mean there is no possibility where you can get stucked
+          return false;
+        }
+
+        //if going LEFT ist not allowed try going DOWN
+        nextDirection == Directions.LEFT ? nextDirection = Directions.DOWN : Directions.UP;
+        if(!isMoveAllowed(nextDirection, currentX,currentY)) return true;
+
+        //if going DOWN is not allowed try going UP
+        if(nextDirection == Directions.DOWN) nextDirection = Directions.UP;
+        if(!isMoveAllowed(nextDirection, currentX,currentY)) return true;
+
+        //TODO is this return still needed, i mean there is no possibility where you can get stucked
+        return false;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * checks is there is any collision with a wall
+   * returns false if move is not allowed else true
+   * */
+  bool isMoveAllowed(Directions d, int currentX, currentY)
+  {
+    //Map Measures
+    int currentXPos = currentX;
+    int currentYPos = currentY;
+
+    if(d == Directions.UP && isCollisionPossible(currentYPos, currentXPos, _dummyMap))    return false;
+    if(d == Directions.DOWN && isCollisionPossible(currentYPos, currentXPos, _dummyMap))  return false;
+    if(d == Directions.LEFT && isCollisionPossible(currentYPos, currentXPos, _dummyMap))  return false;
+    if(d == Directions.RIGHT && isCollisionPossible(currentYPos, currentXPos, _dummyMap)) return false;
+    return true;
+  }
+
+  //true if there is an collision with a WALL
+  bool isCollisionPossible(int nextX, int nextY, _dummyMap)
+  {
+    if (_dummyMap[nextY][nextX] == Statics.WALL) return true;
+    return false;
+  }
 }
+
+/**
+ * kollisionsabfrage, returnToHome, ScatterMode, Frightened Mode
+ * alles in die Ghost kommen und die Geister selben haben nur ihre
+ * individuelle Chase/Verfolger Methode und je nach Geist komplex
+ *
+ * UP -> LEFT -> DOWN
+ **/
