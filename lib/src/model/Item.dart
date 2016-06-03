67798dbba51f7dbd6b73371c2c476d26ac0224a0
Niklas <nikkla@web.de>
2016-06-03 12:03:16 +0200

    PowerPill muss nun eingesammelt werden für Win

--- lib/src/model/Item.dart
+++ lib/src/model/Item.dart
@@ -3,18 +3,28 @@ part of pacmanLib;
 class Item extends GameElement {
   bool _visible = false;
   int _score = 0;
+  static int _items = 0;
+  static int _itemsPickedUp = 0;
   PacmanGameModel _model;
 
   Item(int x, int y, bool collPlayer, bool collGhost, bool visible, int score,
       PacmanGameModel model)
-      : super(x, y, collPlayer, collGhost),
-        this._visible = visible,
-        this._score = score,
-        this._model = model;
+      : super(x, y, collPlayer, collGhost) {
+    this._visible = visible;
+    this._score = score;
+    this._model = model;
+    if(!(this is Cherry)) _items++;
+  }
+
   /**
    * the [Item] is picked up. Make [Item] invisible
    */
   void pickUp() {
-    _visible = false;
+    if(_visible) {
+      if(!(this is Cherry)) _itemsPickedUp++;
+      _visible = false;
+      if(_itemsPickedUp == _items) _model.gameWon();
+    }
+
   }
 }
